8665d4ec475bafc7efb2a09467caa1e07a2b959c
Kmisiz <khanhm1612.ptit@gmail.com>
2025-05-16 17:23:50 +0700

    update_code

---
+++ 2025_05_09/Thuc_hanh_tren_lop/script.js
@@ -0,0 +1,20 @@
+alert('Hello world');
+confirm('Are you sure?');
+prompt('What is your name?');
+var full_name="Nguyen Van A";
+var number=10;
+var number2=20;
+if (number > number2) {
+    alert("1 > 2");
+}
+else if (number < number2) {
+    alert("1< 2");
+}
+else {
+    alert("1 = 2");
+}
+var i = 0;
+while (i < 5) {
+    alert("Hello " + i);
+    i++;
+}
